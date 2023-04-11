af25d5188f948e1d661ed4e348233ea36c16df09
Afroza-Khanom <afrozafra.aa@gmail.com>
2023-04-11 23:33:34 +0600

    four router link added

--- src/Components/Header/Header.jsx
+++ src/Components/Header/Header.jsx
@@ -1,10 +1,15 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
+import './Header.css'
 
 const Header = () => {
     return (
-        <div>
-            <h1>this is header</h1>
-        </div>
+       <nav>
+        <Link to='/'>Home</Link>
+        <Link to='statistics'>Statistics</Link>
+        <Link to='appliedJob'>Applied Jobs</Link>
+        <Link to='blog'>Blog</Link>
+       </nav>
     );
 };
 
--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,45 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
+import Header from './Components/Header/Header';
+import Home from './Components/Home/Home';
+import Main from './Components/Layout/Main';
+import AppliedJob from './Components/AppliedJob/AppliedJob';
+import Statistics from './Components/Statistics/Statistics';
+import Blog from './Components/Blog/Blog';
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Main></Main>,
+    children: [
+      {
+        path: '/',
+        element: <Home></Home>
+      },
+      {
+        path:'appliedjob',
+        element: <AppliedJob></AppliedJob>
+      },
+      {
+        path: 'statistics',
+        element: <Statistics></Statistics>
+      },
+      {
+        path: 'blog',
+        element: <Blog></Blog>
+      }
+    ]
+  }
+])
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router}/>
   </React.StrictMode>,
 )
